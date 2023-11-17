be4a44a299a41dc20225cde65131b58525209dad
Lynne Fuyuna <yryuu@outlook.com>
2023-11-17 10:26:59 +0800

    platform: init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Greet from "$lib/Greet.svelte";
+</script>
+
+<h1 class="text-3xl font-bold underline">
+    Welcome to SvelteKit
+</h1>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
+<Greet/>
